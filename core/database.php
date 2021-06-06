fe9e96239ae6776a1dd91a04f77403652dea37d2
DESKTOP-N2EUHAM\sophi <vaniatrovoada@gmail.com>
2021-06-07 00:01:18 +0100

    Cleaning comments

--- core/database.php
+++ core/database.php
@@ -3,34 +3,19 @@
 //ficheiro para ligar à base de dados
 function getConnection() {
     
-    $config = require 'configs/dbconfig.php'; //chamo as configurações do dbconfig.php  
-    extract($config); //o extract cria directamente variáveis para todos os índices que estejam no ficheiro dbconfig.php
-    //extract transforma qualquer array associativo em variáveis
-    
-    //$dsn = sprintf("mysql:host=%s;dbname=%s;port=%d", $host, $name, $port); //indica onde estão os meus dados e como vou acedê-los
+    $config = require 'configs/dbconfig.php'; 
+    extract($config); 
     $dsn = "mysql:host=$host;dbname=$name;port=$port";
 
     try { 
         $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
-        //echo "Connected successfully"; //---->check!
-        return $pdo; //tem que retornar o valor para poder ser usado na function getConnection()
         
+        return $pdo;         
 
     } catch(PDOException $e) {
         die('Error connecting to database' . $e->getMessage());
     }
-
-    
-    // outra forma de fazer a ligação à base de dados
-    /*
-    try {
-        $connection = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $user, $pass);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-    } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    } */
 }
 
